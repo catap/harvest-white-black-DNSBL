bbf69fc97f1a1212a70ccd74ec3d85ea4c07c7c5
Kirill A. Korinsky <kirill@korins.ky>
2024-06-02 17:30:43 +0100

    Include RDNS, switch format to dnset
    Also, avoid poison the data by stop after first used record.

--- email-received-ips.pl
+++ email-received-ips.pl
@@ -2,18 +2,22 @@
 
 =head1 NAME
 
-email-received-ips.pl - Dumps received IPs from an email
+email-received-ips.pl - Dumps received IPs and RNDS from an email
 
 =head1 SYNOPSIS
 
-It expects an email or emails which is separated as 0x0a0x0a0x0cx0a to STDIN and
-prints all IPs found in Received headers to STDOUT.
+It expects an email or emails which is separated as 0x0a0x0a0x0cx0a to
+STDIN and prints the first public IP and its RDNS which is found in
+Received headers.
 
 It compatible with doveadm, so you may run:
 
   doveadm fetch hdr -A mailbox Junk SINCE 7d | email-received-ips.pl | sort -u
 
-to get IPs associeted with emails in Junk folders of all accounts for last week.
+to get IPs and RDNS records associeted with emails in Junk folders of
+all accounts for the last week.
+
+Output format is compatible with rbldnsd dnset zone.
 
 =cut
 
@@ -35,7 +39,10 @@ use Net::IP;
             my $data = parse_received($_);
             my $ip = $data->{ip} or next;
             my $ip = new Net::IP($ip) or next;
-            say $data->{ip} if $ip->iptype() eq 'PUBLIC';
+            $ip->iptype() eq 'PUBLIC' or next;
+            say join ".", reverse split /\./, $data->{ip};
+            say $data->{rdns} if $data->{rdns};
+            last;
         }
     }
 }
